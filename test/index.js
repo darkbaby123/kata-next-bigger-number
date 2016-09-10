151207d3e58af1b159a19ebe9e0203f06c6468ce
David Chen <darkbaby123@gmail.com>
2016-09-10 22:00:07 +0800

    improve test

--- test/index.js
+++ test/index.js
@@ -1,18 +1,20 @@
 const nextBigger = require('../lib/index')
 
 describe('next bigger number', () => {
-  [
-    [12, 21],
-    [513, 531],
-    [2017, 2071],
-    [2375, 2537],
-    [9, -1],
-    [111, -1],
-    [531, -1],
-    [1234567890, 1234567908],
-  ].forEach(([num, result]) => {
-    it(`${num} -> ${result}`, () => {
-      expect(nextBigger(num)).toEqual(result)
-    })
-  })
+  function testCase({ orig, dest, focus = false }) {
+    const text = `${orig} -> ${dest}`
+    const fn = () => { expect(nextBigger(orig)).toEqual(dest) }
+    focus ? it.only(text, fn) : it(text, fn)
+  }
+
+  // To test one case, use "focus"
+  // testCase({ orig: 12, dest: 21, focus: true })
+  testCase({ orig: 12, dest: 21 })
+  testCase({ orig: 513, dest: 531 })
+  testCase({ orig: 2017, dest: 2071 })
+  testCase({ orig: 2375, dest: 2537 })
+  testCase({ orig: 9, dest: -1 })
+  testCase({ orig: 111, dest: -1 })
+  testCase({ orig: 531, dest: -1 })
+  testCase({ orig: 1234567890, dest: 1234567908 })
 })
